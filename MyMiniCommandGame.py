eca99bb064013944a6dbca1201609f83d780b534
Chris Mappouras <57260161+Pelopedis@users.noreply.github.com>
2020-06-02 09:32:38 +0300

    Update MyMiniCommandGame.py

--- MyMiniCommandGame.py
+++ MyMiniCommandGame.py
@@ -2,7 +2,21 @@ from sys import exit
 from random import randint
 from textwrap import dedent
 
-from Engine import *
+class Engine(object):
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        #printing out last scene
+        current_scene.enter()
 
 class Scene(object):
 
